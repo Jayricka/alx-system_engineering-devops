463b464736f776de6bef6dbb6065d3748dc13b38
jayricka <jayricka17@gmail.com>
2023-09-23 00:05:44 +0300

    config using puppets

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# Puppet manifest to install Flask system-wide
-package { 'python3-flask':
-  ensure => '2.2.2-2ubuntu1.1',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
