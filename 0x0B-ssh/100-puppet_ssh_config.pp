8cbf18aa8252e47e9f31596f66ff451a95189a73
jayricka <jayricka17@gmail.com>
2023-09-25 00:34:03 +0300

    optional

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,9 @@
-# Ensure SSH client configuration file
 file { '/etc/ssh/ssh_config':
   ensure => present,
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
-}
-
-# Include the puppetlabs/stdlib module
-include stdlib
-
-# Configure SSH client
-stdlib::file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  require => File['/etc/ssh/ssh_config'],
-}
-
-stdlib::file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  require => File['/etc/ssh/ssh_config'],
-}
-
-# Notify that changes have been made
-notify { 'SSH client configuration updated':
-  require => [Stdlib::File_line['Declare identity file'], Stdlib::File_line['Turn off passwd auth']],
+  content => "Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no\n",
 }
