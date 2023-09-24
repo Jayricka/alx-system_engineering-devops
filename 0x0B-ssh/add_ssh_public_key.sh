d2ae9fc35effb6cb34a8236ad635140e34ead1e0
jayricka <jayricka17@gmail.com>
2023-09-25 00:31:16 +0300

    optional task

---
+++ 0x0B-ssh/add_ssh_public_key.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This script adds an SSH public key to the authorized_keys file for the current user.
+
+# Set the path to the authorized_keys file
+authorized_keys="$HOME/.ssh/authorized_keys"
+
+# SSH public key to add
+ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
+
+# Add the SSH public key to the authorized_keys file
+echo "$ssh_public_key" >> "$authorized_keys"
+
+# Set the correct permissions
+chmod 700 "$HOME/.ssh"
+chmod 600 "$authorized_keys"
