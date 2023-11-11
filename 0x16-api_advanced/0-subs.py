b6acff077bdfec13e673b5244c15fa7186ee2dae
jayricka <jayricka17@gmail.com>
2023-11-11 19:09:12 +0300

    apis

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
-"""0-subs"""
+"""
+Function to query subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'CustomUserAgent/1.0'}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        return response.json().get('data', {}).get('subscribers', 0)
-    elif response.status_code == 404:
-        return 0
-    else:
-        print(f"Error: {response.status_code}")
+    """
+    Return the total number of subscribers on a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "my-app/0.1.0 (by /u/ricka)"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json().get("data")
+        return data.get("subscribers")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
-    
+
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print("{:d}".format(subscribers))
