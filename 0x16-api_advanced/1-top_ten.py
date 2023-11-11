d5ba0097d3e294c8cfc002e20f8c2f499eef1ac9
jayricka <jayricka17@gmail.com>
2023-11-11 19:41:47 +0300

    advance api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""1-top_ten"""
-
+"""query titles of first 10 hot posts for a given Reddit subreddit."""
 import requests
+
 
 def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'CustomUserAgent/1.0'}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        [print(post.get('data', {}).get('title')) for post in response.json().get('data', {}).get('children', [])]
-    elif response.status_code == 404:
-        print(None)
-    else:
-        print(f"Error: {response.status_code}")
+    headers = {"User-Agent": "my-app/0.1.0 (by /u/ricka)"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            print(post.get("data", {}).get("title"))
+    except requests.exceptions.RequestException:
         print(None)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
-    
+
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
