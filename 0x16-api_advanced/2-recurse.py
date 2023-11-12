7f53ae3aa86b243664d3456602fe90c692792bba
jayricka <jayricka17@gmail.com>
2023-11-12 07:44:12 +0300

    advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python3
-"""Recursive function to query the Reddit API and return a list containing
-the titles of all hot articles for a given subreddit."""
+"""
+query the Reddit API for hot articles in a given subreddit.
+"""
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    url = (
-        f"https://www.reddit.com/r/{subreddit}/hot.json?"
-        f"limit=100&after={after}"
-    )
-    headers = {"User-Agent": "my-app/0.1.0 (by /u/your_username)"}
-
-    try:
-        response = requests.get(
-            url, headers=headers, allow_redirects=False
-        )
-        response.raise_for_status()
-
-        data = response.json().get("data", {}).get("children", [])
-
-        if not data:
-            return hot_list
-
-        for post in data:
-            hot_list.append(post.get("data", {}).get("title"))
-
-        after = response.json().get("data", {}).get("after")
-
-        return recurse(subreddit, hot_list, after)
-    except requests.exceptions.RequestException:
+def recurse(subreddit, hot_list=None, after=None):
+    if not is_valid_subreddit(subreddit):
         return None
 
+    if hot_list is None:
+        hot_list = []
 
-if __name__ == "__main__":
+    if after is None:
+        return hot_list
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'after': after, 'limit': 100}
+    headers = {'User-Agent': 'your_user_agent'}
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json()
+    children = data.get('data', {}).get('children', [])
+    titles = [child.get('data', {}).get('title', '') for child in children]
+    hot_list.extend(titles)
+
+    after = data.get('data', {}).get('after')
+    return recurse(subreddit, hot_list, after)
+
+
+def is_valid_subreddit(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'your_user_agent'}
+    response = requests.get(url, headers=headers)
+    return (
+        response.status_code == 200
+        and response.json().get('data', {}).get('display_name') == subreddit
+    )
+
+
+if __name__ == '__main__':
     import sys
 
     if len(sys.argv) < 2:
