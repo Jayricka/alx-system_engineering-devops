d437d258ef15225a3cb31691f12938ea04660699
jayricka <jayricka17@gmail.com>
2023-11-12 20:24:15 +0300

    debug web

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,35 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+file { '/etc/apache2/apache2.conf':
+  ensure  => 'present',
+  content => '
+    DefaultRuntimeDir /var/run/apache2
+  ',
+  notify  => Service['apache2'],
+}
 
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+file { '/etc/apache2/mods-available/mpm_prefork.conf':
+  ensure  => 'present',
+  content => '
+    <IfModule mpm_prefork_module>
+        StartServers             5
+        MinSpareServers          5
+        MaxSpareServers          10
+        MaxClients              150
+        MaxRequestsPerChild     0
+    </IfModule>
+  ',
+  notify  => Service['apache2'],
+}
+
+file { '/etc/apache2/mods-enabled/mpm.conf':
+  ensure => 'link',
+  target => '/etc/apache2/mods-available/mpm_prefork.conf',
+}
+
+service { 'apache2':
+  ensure  => 'running',
+  enable  => true,
+}
+
+notify { 'Debug Message':
+  message => "Puppet ran successfully up to this point.",
 }
