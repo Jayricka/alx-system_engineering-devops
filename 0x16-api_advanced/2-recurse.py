7ea393574b525fc423cf4bdffa9fd7bb7beb734b
jayricka <jayricka17@gmail.com>
2023-11-12 07:51:47 +0300

    debugging

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-query the Reddit API for hot articles in a given subreddit.
+recursive function to query Reddit API for hot articles in a given subreddit.
 """
 
 import requests
 
 
 def recurse(subreddit, hot_list=None, after=None):
-    if not is_valid_subreddit(subreddit):
-        return None
-
     if hot_list is None:
         hot_list = []
-
-    if after is None:
-        return hot_list
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     params = {'after': after, 'limit': 100}
@@ -24,23 +18,16 @@ def recurse(subreddit, hot_list=None, after=None):
     if response.status_code != 200:
         return None
 
-    data = response.json()
-    children = data.get('data', {}).get('children', [])
+    data = response.json().get('data', {})
+    if not data or 'children' not in data:
+        return hot_list
+
+    children = data['children']
     titles = [child.get('data', {}).get('title', '') for child in children]
     hot_list.extend(titles)
 
-    after = data.get('data', {}).get('after')
+    after = data.get('after')
     return recurse(subreddit, hot_list, after)
-
-
-def is_valid_subreddit(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'your_user_agent'}
-    response = requests.get(url, headers=headers)
-    return (
-        response.status_code == 200
-        and response.json().get('data', {}).get('display_name') == subreddit
-    )
 
 
 if __name__ == '__main__':
@@ -50,7 +37,7 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         result = recurse(sys.argv[1])
-        if result is not None:
+        if result:
             print(len(result))
         else:
             print("None")
