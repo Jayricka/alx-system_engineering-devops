f973ed045f24c6dcf29a77bce1c6cf42a384092c
jayricka <jayricka17@gmail.com>
2023-11-27 16:03:36 +0300

    automate configuration with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,48 @@
+# 7-puppet_install_nginx_web_server.pp
+
 # Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Configure Nginx for port 80
-file { '/etc/nginx/sites-available/default':
-  ensure => file,
-  content => "
-server {
-  listen 80;
-  root /var/www/html;
-  index index.html;
-
-  location / {
-    return 200 'Hello World!';
-  }
-
-  location /redirect_me {
-    return 301 'https://www.linkedin.com/in/rikagachanja/';
-  }
-}
-  ",
-  notify => Service['nginx'],
-}
-
-# Create a symlink to enable the site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify => Service['nginx'],
-}
-
-# Remove the default Nginx configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure => absent,
-  notify => Service['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
+# Ensure Nginx service is running
 service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  require => File['/etc/nginx/sites-enabled/default'],
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
 
+# Create a basic HTML file with Hello World content
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Configure Nginx to listen on port 80
+file { '/etc/nginx/sites-available/default':
+  ensure => present,
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+",
+  notify => Service['nginx'],
+}
+
+# Restart Nginx when the configuration changes
+exec { 'nginx-restart':
+  command     => '/etc/init.d/nginx restart',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
+}
